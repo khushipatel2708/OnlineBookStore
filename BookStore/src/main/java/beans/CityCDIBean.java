634b00dcb30879bcd11f63f86af9d90192a65edd
khushipatel2708 <pkhush818@gmail.com>
2025-12-07 16:09:37 +0530

    chanages

--- BookStore/src/main/java/beans/CityCDIBean.java
+++ BookStore/src/main/java/beans/CityCDIBean.java
@@ -26,7 +26,6 @@ public class CityCDIBean implements Serializable {
     }
 
     public Collection<City> getAllCities() {
-        if (isAdmin()) return null;
         return adminClient.getAllCities(Collection.class);
     }
 
