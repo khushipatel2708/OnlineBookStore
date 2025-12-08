1565043fe1ddd97ca9b3b173f3aeb672397fb326
khushipatel2708 <pkhush818@gmail.com>
2025-12-08 19:18:39 +0530

    book type pagignation

--- BookStore/src/main/java/beans/BooktypeCDIBean.java
+++ BookStore/src/main/java/beans/BooktypeCDIBean.java
@@ -6,7 +6,10 @@ import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Named("booktypeBean")
 @SessionScoped
@@ -106,4 +109,52 @@ public class BooktypeCDIBean implements Serializable {
 
     public boolean isEditMode() { return editMode; }
     public void setEditMode(boolean editMode) { this.editMode = editMode; }
+    
+    
+      private int pageSize = 5;      // Number of rows per page
+    private int pageNumber = 1;    // Current page
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public int getTotalPages() {
+        int totalGroups = getAllBooktypes().size();
+        return (int) Math.ceil((double) totalGroups / pageSize);
+    }
+
+    public Collection<Booktype> getPagedGroups() {
+        List<Booktype> all = new ArrayList<>(getAllBooktypes());
+        int fromIndex = (pageNumber - 1) * pageSize;
+        int toIndex = Math.min(fromIndex + pageSize, all.size());
+        if (fromIndex > all.size()) {
+            return Collections.emptyList();
+        }
+        return all.subList(fromIndex, toIndex);
+    }
+
+    public void nextPage() {
+        if (pageNumber < getTotalPages()) {
+            pageNumber++;
+        }
+    }
+
+    public void previousPage() {
+        if (pageNumber > 1) {
+            pageNumber--;
+        }
+    }
+
 }
