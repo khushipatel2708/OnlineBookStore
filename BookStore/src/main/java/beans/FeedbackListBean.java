c60adde6862bc3e15d4854142c26166e5fab9118
YESHA2005 <179323889+khushipatel2708@users.noreply.github.com>
2025-12-09 13:29:13 +0530

    changes

--- BookStore/src/main/java/beans/FeedbackListBean.java
+++ BookStore/src/main/java/beans/FeedbackListBean.java
@@ -5,24 +5,62 @@ import Entity.Feedback;
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
-import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-@Named
-@RequestScoped
-public class FeedbackListBean {
+@Named("feedbackListBean")
+@SessionScoped
+public class FeedbackListBean implements Serializable {
 
     @Inject
     private UserSessionBeanLocal userEJB;
 
-    private List<Feedback> feedbackList;
+    private List<Feedback> allFeedbacks;
+
+    private int pageSize = 5;     // rows per page
+    private int pageNumber = 1;   // current page
 
     @PostConstruct
     public void init() {
-        feedbackList = userEJB.getAllFeedbacks();
+        allFeedbacks = userEJB.getAllFeedbacks();
     }
 
-    public List<Feedback> getFeedbackList() {
-        return feedbackList;
+    public List<Feedback> getPagedFeedbacks() {
+        List<Feedback> list = new ArrayList<>(allFeedbacks);
+
+        int from = (pageNumber - 1) * pageSize;
+        int to = Math.min(from + pageSize, list.size());
+
+        if (from > list.size()) {
+            return Collections.emptyList();
+        }
+        return list.subList(from, to);
+    }
+
+    public int getTotalPages() {
+        return (int) Math.ceil((double) allFeedbacks.size() / pageSize);
+    }
+
+    public void nextPage() {
+        if (pageNumber < getTotalPages()) {
+            pageNumber++;
+        }
+    }
+
+    public void previousPage() {
+        if (pageNumber > 1) {
+            pageNumber--;
+        }
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+    
+    public int getPageSize() {
+        return pageSize;
     }
 }
